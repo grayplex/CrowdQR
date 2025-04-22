b13c45420df468a590c7a7cf908ab1a10bdb71c8
grayplex <admin@grayplex.com>
2025-04-22 15:17:25 -0500

    Refactor migration namespace casing
    Updated the namespace for the InitialCreate migration class from `CrowdQR.API.Migrations` to `CrowdQR.Api.Migrations` to adhere to naming conventions.

--- src/CrowdQR.API/Migrations/20250419173058_InitialCreate.cs
+++ src/CrowdQR.API/Migrations/20250419173058_InitialCreate.cs
@@ -4,7 +4,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace CrowdQR.API.Migrations
+namespace CrowdQR.Api.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
