f887aa7396d68fac75b28acac65e79db39d78470
grayplex <admin@grayplex.com>
2025-04-22 19:22:57 -0500

    Refactor namespaces and update User model
    This commit refactors namespaces from `CrowdQR.API` to `CrowdQR.Api` across multiple files. It adds new columns to the `User` table in the `20250423001829_AddUserEmailAndPassword` migration, enhancing support for email verification and password management. The `BuildTargetModel` method and `CrowdQRContextModelSnapshot` have been updated to reflect these changes. Additionally, the HTTP request in the `http` file has been modified to ensure correct routing of API calls.

--- src/CrowdQR.API/Migrations/20250423001829_AddUserEmailAndPassword.Designer.cs
+++ src/CrowdQR.API/Migrations/20250423001829_AddUserEmailAndPassword.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CrowdQR.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CrowdQR.Api.Migrations
 {
     [DbContext(typeof(CrowdQRContext))]
-    partial class CrowdQRContextModelSnapshot : ModelSnapshot
+    [Migration("20250423001829_AddUserEmailAndPassword")]
+    partial class AddUserEmailAndPassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
