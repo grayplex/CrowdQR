45294be48c80b90c719effb5e17b4773bb1661c4
grayplex <admin@grayplex.com>
2025-05-28 08:19:38 -0500

    Refactor vote DTOs and update related services and tests

--- src/CrowdQR.Shared/Models/DTOs/VoteDto.cs
+++ src/CrowdQR.Shared/Models/DTOs/VoteDto.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace CrowdQR.Shared.Models.DTOs;
+﻿namespace CrowdQR.Shared.Models.DTOs;
 
 /// <summary>
 /// Data transfer object representing a vote on a request.
@@ -28,20 +26,3 @@ public class VoteDto
     public DateTime CreatedAt { get; set; }
 }
 
-/// <summary>
-/// Data transfer object for creating a new vote.
-/// </summary>
-public class VoteCreateDto
-{
-    /// <summary>
-    /// The ID of the user casting the vote.
-    /// </summary>
-    [Required]
-    public int UserId { get; set; }
-
-    /// <summary>
-    /// The ID of the request being voted for.
-    /// </summary>
-    [Required]
-    public int RequestId { get; set; }
-}
