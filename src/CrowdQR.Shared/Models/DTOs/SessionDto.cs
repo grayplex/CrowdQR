ea6b619dd8097fc35814839b9538cbed14c283ed
grayplex <admin@grayplex.com>
2025-05-28 08:23:22 -0500

    Refactor session DTOs and update related services and tests

--- src/CrowdQR.Shared/Models/DTOs/SessionDto.cs
+++ src/CrowdQR.Shared/Models/DTOs/SessionDto.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace CrowdQR.Shared.Models.DTOs;
+﻿namespace CrowdQR.Shared.Models.DTOs;
 
 /// <summary>
 /// Data transfer object representing a user session.
@@ -36,28 +34,4 @@ public class SessionDto
     /// When the session was last active. This is used to determine session validity and for cleanup purposes.
     /// </summary>
     public DateTime LastSeen { get; set; }
-}
-
-/// <summary>
-/// Data transfer object for creating or updating a session.
-/// </summary>
-public class SessionCreateDto
-{
-    /// <summary>
-    /// The ID of the user for this session.
-    /// </summary>
-    [Required]
-    public int UserId { get; set; }
-
-    /// <summary>
-    /// The ID of the event for this session.
-    /// </summary>
-    [Required]
-    public int EventId { get; set; }
-
-    /// <summary>
-    /// The client IP address (for rate limiting and security).
-    /// </summary>
-    [StringLength(45)]
-    public string? ClientIP { get; set; }
 }
